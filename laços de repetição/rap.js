d9f5bb8f7bf8c9d0e290ce7bab4279a7995e16e5
rafael <djalmasantos839@gmail.com>
2019-10-11 11:50:35 -0300

    reconfigurando programa VAMOS CONTAR

--- laços de repetição/rap.js
+++ laços de repetição/rap.js
@@ -6,13 +6,19 @@ function contar() {
     if (start.value.length == 0 || end.value.length == 0 || step.value.length == 0) {
         alert('[ERRO] prenencha todos os campos')
     } else {
-       res.innerHTML= 'contando:'
-       var i= Number (start.value)
-       var f= Number (end.value)
-       var p= Number (step.value)
-       
-       for(c=i; c<=f; c+=p) {
-           res.innerHTML += ` \u{1F449} ${c} `
-       }
-    }  res.innerHTML +=`\u{1F595}`
+        res.innerHTML = 'contando:'
+        var i = Number(start.value)
+        var f = Number(end.value)
+        var p = Number(step.value)
+        if (i < f) {
+            for (c = i; c <= f; c += p) {
+                res.innerHTML += ` \u{1F449} ${c} `
+            }
+        } else {
+            for (c = i; c >= f; c -= p) {
+                res.innerHTML += ` \u{1F449} ${c} `
+            }
+        }
+        res.innerHTML+= `\u{1F3C1}`
+    } 
 }
