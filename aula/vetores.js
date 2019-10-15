ebbf25b5c532dc9e053055c3f9d1f436e77ba40b
Rafael <djalmasantos839@gmail.com>
2019-10-15 11:38:03 -0300

    Aula de vetores adicionada

---
+++ aula/vetores.js
@@ -0,0 +1,17 @@
+let num= [2, 8, 9, -3, 5, 1, 6]
+num.sort()
+let pos=num.indexOf(2)
+console.log(`o valor 2 esta na posição ${pos}`)
+/*for(let pos= 0; pos<num.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}*/
+/*num.push(7)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)*/ 
+
+/*---modo mais simplificado para array---*/
+/*for(let pos in num) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}*/
+
