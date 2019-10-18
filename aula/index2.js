cadc7b4c87a1eeb0c49d2d108199cc840da87e95
Eunice Santos <djalmasantos839@gmail.com>
2019-10-17 23:15:25 -0300

    modificações feita eexercicio adicionado

--- aula/index2.js
+++ aula/index2.js
@@ -1,25 +1,39 @@
 let num = document.querySelector('#nume')
-let an = documente.querySelector('#num')
-let res= document
+let anali = document.querySelector('#num')
+let res= document.querySelector('#res')
 let valores= []
-function numero (n) {
+function numero(n) {
     if(Number (n) >=1 && Number (n) <=100) {
         return true
     } else {
         return false
     }
 }
-function lista (n, l) {
-    if(l.indexOf(Number(n) != -1)) {
+function lista(n, l) {
+    if(l.indexOf(Number(n)) != -1) {
         return true
     } else {
         return false
     }
 }
 function adicionar() {
-    if (numero(num.value) && lista(num.value, valores)) {
-        
+    if (numero(num.value) && !lista(num.value, valores)) {
+        valores.push(Number(num.value))
+        var item = document.createElement('option')
+        item.text= `valor ${num.value} adicionado`
+        anali.appendChild(item)
     } else {
         alert('Valor inválido ou já encontrado na lista')
+    }
+    num.value= ''
+    num.focus()
+}
+function finalizar() {
+    if(valores.length == 0) {
+        alert('Nenhum valor adicionado')
+    } else{
+        let tot = valores.length
+        res.innerHTML= ''
+        res.innerHTML+= `<p>Ao todo, temos ${tot} números cadastrado</p>`
     }
 }
